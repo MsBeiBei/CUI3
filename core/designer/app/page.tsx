3499a3851501d180d57ebe29af2e1f6ea1d17dda
BeiBei <Ms_BeiBei@yeah.net>
2023-06-05 18:40:56 +0800

    初始化

---
+++ core/designer/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            22
+        </main>
+    )
+}
--- core/index.ts
+++
@@ -1,6 +0,0 @@
-let num: number = 1
-let str: string = 'hello'
-let student: { name: string; age: number }  = { name: '小明', age: 12}
-
-num = str = student = null
-num = str = student = undefined
