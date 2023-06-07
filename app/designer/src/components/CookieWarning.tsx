72540483cf01db7e9f9f294e7180a420a8f16589
BeiBei <Ms_BeiBei@yeah.net>
2023-06-07 18:56:22 +0800

    初始化

---
+++ app/designer/src/components/CookieWarning.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react"
+
+const CookieWarning: FC<{}> = () => {
+    return (
+        <div
+            style={{
+                margin: '1em',
+            }}
+        >
+            You do not have cookies enabled for this site. In order to interact with
+            the Linode Cloud Manager, please enable cookies for cloud.linode.com and
+            login.linode.com
+            <a
+                href="https://orteil.dashnet.org/cookieclicker/"
+                target="_blank"
+                aria-describedby="external-site"
+                rel="noopener noreferrer"
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    margin: '1em',
+                }}
+            >
+                :)
+            </a>
+        </div>
+    );
+}
+
+export default CookieWarning
